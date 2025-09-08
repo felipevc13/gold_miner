122245c3d8a5c37f038cfada46021da77329c3ca
felipevc13 <felipevc13@gmail.com>
2025-09-08 16:17:44 -0300

    lp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -269,9 +269,10 @@ export default defineNuxtConfig({
 
   // Tailwind CSS configuration
   tailwindcss: {
-    cssPath: ['~/assets/css/tailwind.css', { injectPosition: 0 }],
+    cssPath: '~/assets/css/tailwind.css',
     configPath: 'tailwind.config',
     exposeConfig: false,
+    injectPosition: 0,
     viewer: false,
     config: {}
   },
